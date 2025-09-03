6c237272a5f0eda978c3f792f1c3bde535a6fc93
sudhakarzox <p.sudhakar963@gmail.com>
2025-09-03 20:24:28 +0530

    fix4

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import { Router } from 'react-router';
 import './App.css';
+import { Route, Routes } from 'react-router';
+import  Timer  from './component/Timer';
+import Toggle from './component/Toggle';
+import Bottle from './component/Bottle';
 import All from './component/All';
   
 function App() {
   return (
     <div className="App">
-      <Router>
-        <All/>
-      </Router>
+     <Routes>
+     <Route path="/" element={<All/>}/>
+      <Route path="/to" element={<Toggle/>}/>
+      <Route path="/t" element={<Timer/>}/>
+      <Route path="/b" element={<Bottle/>}/>
+      <Route path="/a" element={<All/>}/>
+     </Routes>
     </div>
   );
 }
